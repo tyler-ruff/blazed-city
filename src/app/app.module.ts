353eba81e36b2122fb9308d83b6d8b4e954f64cc
Tyler Ruff <hello@blazed.space>
2022-11-18 01:37:46 -0500

    BLZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CommonModule } from '@angular/common';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -11,6 +13,13 @@ import { FooterComponent } from './components/footer/footer.component';
 import { LazyLoadDirective } from './lazyload.directive';
 import { AboutComponent } from './pages/about/about.component';
 import { ExploreComponent } from './pages/explore/explore.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -25,14 +34,24 @@ import { ExploreComponent } from './pages/explore/explore.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CommonModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'blz-one-9e383',
+    appId: '1:817319905653:web:d2bf92492a974da38bab3c',
+    databaseURL: 'https://blz-one-9e383-default-rtdb.firebaseio.com',
+    storageBucket: 'blz-one-9e383.appspot.com',
+    locationId: 'us-east4',
+    apiKey: 'AIzaSyDJNpSgZqhn1_ipvk54wsU11UIE1BoYIfc',
+    authDomain: 'blz-one-9e383.firebaseapp.com',
+    messagingSenderId: '817319905653',
+    measurementId: 'G-07B9ZVVPEL',
+  },
   production: false
 };
 
